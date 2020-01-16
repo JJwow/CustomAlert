f086f2886ccadadf07c12174fe369960dedca01e
wjj <junjie.wang@ly.com>
2020-01-16 10:56:45 +0800

    no message

--- Example/Pods/Target Support Files/CustomAlert/CustomAlert-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CustomAlert : NSObject
-@end
-@implementation PodsDummy_CustomAlert
-@end
--- Example/Pods/Target Support Files/Pods-CustomAlert_Tests/Pods-CustomAlert_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CustomAlert_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_CustomAlert_Tests
-@end
